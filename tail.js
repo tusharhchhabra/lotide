5608a0f752045854a7e5a96ee284521c806921e8
Tushar Chhabra <tusharhchhabra@gmail.com>
2023-08-22 18:44:41 -0700

    Tail method

---
+++ tail.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(arr) {
+  if (!Array.isArray(arr)) {
+    return undefined;
+  } else if (arr.length < 2) {
+    return [];
+  }
+
+  return arr.slice(1);
+};
+
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
+
+assertEqual(tail([]).length, 0);
