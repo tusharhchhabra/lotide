3f38a0e9c19b3872190aad3a8ea0c1eda9e5a3a7
Tushar Chhabra <tusharhchhabra@gmail.com>
2023-08-23 02:43:11 -0700

    Implement without

---
+++ without.js
@@ -0,0 +1,63 @@
+const without = (source, itemsToRemove) => {
+  if (!Array.isArray(source) || !Array.isArray(itemsToRemove)) {
+    console.log("Error: Arguments must be arrays.");
+    return;
+  }
+
+  let resultArray = [];
+
+  for (const sourceItem of source) {
+    let shouldExcludeItem = false;
+
+    for (const itemToRemove of itemsToRemove) {
+      if (itemToRemove === sourceItem) {
+        shouldExcludeItem = true;
+      }
+    }
+    
+    if (!shouldExcludeItem) {
+      resultArray.push(sourceItem);
+    }
+  }
+
+  return resultArray;
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+    console.log("Please pass 2 arrays as arguments.");
+    return undefined;
+  }
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  let elementsAreIdentical = true;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      elementsAreIdentical = false;
+      break;
+    }
+  }
+
+  return elementsAreIdentical;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+    console.log(`🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+};
+
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+assertArraysEqual(without([1, 3], []), [1, 3]);
+assertArraysEqual(without([], [1]), []);
+assertArraysEqual(without([], []), []);
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]);
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
