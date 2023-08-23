8deb7f31a37b9c18f3a8f83be4d8c874333f6044
Tushar Chhabra <tusharhchhabra@gmail.com>
2023-08-23 02:20:27 -0700

    Implement assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,33 @@
+const eqArrays = function(arr1, arr2) {
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+    console.log("Please pass 2 arrays as arguments.");
+    return undefined;
+  }
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  let elementsAreIdentical = true;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      elementsAreIdentical = false;
+      break;
+    }
+  }
+
+  return elementsAreIdentical;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+    console.log(`🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 3], [1, 2, 3]);
+assertArraysEqual([], [1]);
+assertArraysEqual([], []);
