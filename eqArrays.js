48b8c250fb8193cbde914b3f9f2d2ffa3108cbe0
Tushar Chhabra <tusharhchhabra@gmail.com>
2023-08-22 19:11:31 -0700

    Array equality check method

---
+++ eqArrays.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+    console.log("Please pass 2 arrays as arguments.");
+    return undefined;
+  }
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  let elementsAreIdentical = true;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      elementsAreIdentical = false;
+      break;
+    }
+  };
+
+  return elementsAreIdentical;
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2]), false);
+assertEqual(eqArrays("hello", [1, 2]), undefined);
+assertEqual(eqArrays("hello"), undefined);
