5a88c1263da2c34374b5b8f544ca4f93fb9eabff
Tushar Chhabra <tusharhchhabra@gmail.com>
2023-08-23 22:32:53 -0700

    Implement countLetters

---
+++ countLetters.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = (string) => {
+  const results = {};
+
+  for (const char of string) {
+    if (results[char]) {
+      results[char] += 1;
+    } else {
+      results[char] = 1;
+    }
+  }
+
+  return results;
+};
+
+
+let count = countLetters("Lighthouse Labs");
+
+assertEqual(count["L"], 2);
+assertEqual(count["s"], 2);
+assertEqual(count["e"], 1);
