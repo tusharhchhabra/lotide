33fd65edc058074b10501aee1920069491b88928
Tushar Chhabra <tusharhchhabra@gmail.com>
2023-08-29 00:19:58 -0700

    Refactor tail

--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(arr) {
   if (!Array.isArray(arr)) {
     return undefined;
@@ -16,14 +8,4 @@ const tail = function(arr) {
   return arr.slice(1);
 };
 
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-
-assertEqual(tail([]).length, 0);
+module.exports = tail;
---
+++ test/tailTest.js
@@ -0,0 +1,13 @@
+const tail = require("../tail");
+const assertEqual = require('../assertEqual');
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
+
+assertEqual(tail([]).length, 0);
