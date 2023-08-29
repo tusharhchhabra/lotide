f016355455b70dd57c7d2963a17e0c6146380aa6
Tushar Chhabra <tusharhchhabra@gmail.com>
2023-08-29 00:30:30 -0700

    Refactor middle

--- middle.js
+++ middle.js
@@ -1,33 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
-    console.log("Please pass 2 arrays as arguments.");
-    return undefined;
-  }
-
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  let elementsAreIdentical = true;
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      elementsAreIdentical = false;
-      break;
-    }
-  }
-
-  return elementsAreIdentical;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅ Assertion Passed: [${arr1}] === [${arr2}]`);
-  } else {
-    console.log(`🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
-  }
-};
-
-
 const middle = (arr) => {
   if (!Array.isArray(arr)) {
     console.log("Please pass an array as the argument.");
@@ -46,9 +16,4 @@ const middle = (arr) => {
   }
 };
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
