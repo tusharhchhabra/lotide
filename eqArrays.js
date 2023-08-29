54a38726d11f9fd458c73ce9de6d4f85ef516265
Tushar Chhabra <tusharhchhabra@gmail.com>
2023-08-29 00:23:04 -0700

    Refactor eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(arr1, arr2) {
   if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
     console.log("Please pass 2 arrays as arguments.");
@@ -27,7 +19,4 @@ const eqArrays = function(arr1, arr2) {
   return elementsAreIdentical;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2]), false);
-assertEqual(eqArrays("hello", [1, 2]), undefined);
-assertEqual(eqArrays("hello"), undefined);
+module.exports = eqArrays;
