7e3732059aa454c01a0c6334cea6cf2240ee53c1
Tushar Chhabra <tusharhchhabra@gmail.com>
2023-08-29 00:28:02 -0700

    Refactor assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,23 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
-    console.log("Please pass 2 arrays as arguments.");
-    return undefined;
-  }
-
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  let elementsAreIdentical = true;
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      elementsAreIdentical = false;
-      break;
-    }
-  }
-
-  return elementsAreIdentical;
-};
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
@@ -27,7 +8,4 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 3], [1, 2, 3]);
-assertArraysEqual([], [1]);
-assertArraysEqual([], []);
+module.exports = assertArraysEqual;
