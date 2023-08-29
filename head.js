c02638dcc8b30d677615a286abacaaf52b69348d
Tushar Chhabra <tusharhchhabra@gmail.com>
2023-08-29 00:16:27 -0700

    Lotide Refactor

--- head.js
+++ head.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const head = function(arr) {
   if (!Array.isArray(arr) || arr.length === 0) {
     return undefined;
@@ -14,6 +6,4 @@ const head = function(arr) {
   return arr[0];
 }
 
-assertEqual(head("hello"), undefined);
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
